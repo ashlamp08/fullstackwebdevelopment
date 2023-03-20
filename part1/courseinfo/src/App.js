f2143cddc8a5d72b27cb26b1b97a426b3fbed5ec
ashlamp08 <dhungana.ashok08@gmail.com>
2023-03-20 13:58:53 +0200

    part 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,6 +4,16 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    <div>
+      <Part part={props.parts[0]} exercises={props.exercises[0]} />
+      <Part part={props.parts[1]} exercises={props.exercises[1]} />
+      <Part part={props.parts[2]} exercises={props.exercises[2]} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
     <p>
       {props.part} {props.exercises}
     </p>
@@ -26,9 +36,10 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content
+        parts={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
       <Total total={exercises1 + exercises2 + exercises3} />
     </>
   );
