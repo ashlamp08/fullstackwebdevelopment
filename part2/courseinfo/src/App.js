5feb0ba89a5d14be4292376644f47fede29036a4
ashlamp08 <dhungana.ashok08@gmail.com>
2023-03-24 13:52:31 +0200

    part 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,34 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Total = ({ sum }) => <b>total of exercises {sum}</b>;
-
-const Part = ({ part }) => (
-  <li>
-    {part.name} {part.exercises}
-  </li>
-);
-
-const Course = ({ course }) => {
-  const parts = course.parts;
-  const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={parts} />
-      <Total sum={sum} />
-    </div>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <ul>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </ul>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
